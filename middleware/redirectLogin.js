dc731e4a6f22cbd3f6220a6d5a726a0d178019bf
ibheehe <ibrahimasif25@gmail.com>
2025-12-08 00:49:34 +0000

    changes

--- middleware/redirectLogin.js
+++ middleware/redirectLogin.js
@@ -1,12 +1,7 @@
-// Get app prefix from environment (VM uses /usr/379, local uses '')
-const APP_PREFIX = process.env.APP_PREFIX || '';
-
-const redirectLogin = (req, res, next) => {
+module.exports = function redirectLogin(req, res, next) {
+    const APP_PREFIX = process.env.APP_PREFIX || ""; 
     if (!req.session.userId) {
-        // Always redirect to the correct login URL
         return res.redirect(`${APP_PREFIX}/users/login`);
     }
     next();
 };
-
-module.exports = redirectLogin;
